66f62b96995670d0c0460eb7da8028b9d39929c9
Michal Zalecki <michal@michalzalecki.com>
2015-04-07 23:24:57 +0200

    menu

--- src/com/michalzalecki/view/RootController.java
+++ src/com/michalzalecki/view/RootController.java
@@ -1,10 +1,25 @@
 package com.michalzalecki.view;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.input.KeyEvent;
 
 /**
  * Created by Michal on 2015-03-21.
  */
 public class RootController {
+    public void close(ActionEvent actionEvent) {
+        Platform.exit();
+    }
+
+
+    public void about(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("O programie");
+        alert.setHeaderText("Kalkulator programisty");
+        alert.setContentText("Autor: Michał Załęcki");
+        alert.show();
+    }
 }
